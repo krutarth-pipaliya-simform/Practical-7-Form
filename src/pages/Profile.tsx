546d0126c1703b7faff8a607b23cd52c791256bd
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-06-12 15:42:37 +0530

    feat: add protection on profile route

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 export const Profile = () => {
+    const navigate = useNavigate();
+    if (localStorage.getItem("isLoggedIn") !== "true") navigate("/signup");
     return <div>Profile</div>;
 };
